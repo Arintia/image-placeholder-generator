a0347eb4e0f1b2dab3fbe335710d438be01bc28c
Arintia <arantiabusiness@gmail.com>
2022-04-23 16:57:15 +0300

    Add font size button active state

--- src/components/FontSizeButton.js
+++ src/components/FontSizeButton.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setFontSize } from '../redux/image/ImageSlice';
 
 function FontSizeButton({fontSize, sizeName}) {
     const dispatch = useDispatch();
-
+    const canvasFontSize = useSelector(state => state.image.canvasFontSize);
+    
     return (
         <button 
             value={fontSize} 
             type="button" 
-            className="w-11 justify-center text-white bg-gray-400 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            className={
+                canvasFontSize === fontSize 
+                ? 
+                "w-11 justify-center text-white bg-blue-700 hover:bg-blue-600 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2"
+                :
+                "w-11 justify-center text-white bg-gray-400 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            }
             onClick={(e) => dispatch(setFontSize(e.target.value))}
         >
             {sizeName}
