5593211866e6639608da03ffe09ae915481f6404
Arintia <arantiabusiness@gmail.com>
2022-04-23 16:53:07 +0300

    Add download button

--- src/App.js
+++ src/App.js
@@ -62,6 +62,14 @@ function App() {
     }
   }
 
+  const convertCanvasToPng = (e) => {
+    const canvas = document.getElementById("image-canvas");
+    const image = canvas.toDataURL("image/png");
+    const downloadBtn = document.getElementById("download-canvas");
+    downloadBtn.setAttribute("download", "placeholder");
+    downloadBtn.setAttribute('href', image);
+  }
+
   return (
     <React.Fragment>
       <main className="w-screen h-screen flex flex-col items-center justify-center relative bg-gray-100 dark:bg-gray-800">
@@ -170,7 +178,14 @@ function App() {
                 height={canvasHeight}
               />
             </div>
-            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Download Image</button>
+            <a 
+              id="download-canvas"
+              type="button" 
+              onClick={convertCanvasToPng} 
+              className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              Download Image
+            </a>
           </div>
         </div>
       </main>
