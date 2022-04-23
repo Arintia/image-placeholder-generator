1fdd37e439ba763cc8853a3cef82c8d4b7a22351
Arintia <arantiabusiness@gmail.com>
2022-04-23 14:41:21 +0300

    Improve SEO

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center relative bg-gray-100 dark:bg-gray-800">
+    <main className="w-screen h-screen flex items-center justify-center relative bg-gray-100 dark:bg-gray-800">
       <label htmlFor="theme-switcher" className="swap swap-rotate absolute right-5 top-5">
         <input type="checkbox" id="theme-switcher" onChange={handleThemeSwitch} />
         <svg id="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
@@ -77,7 +77,7 @@ function App() {
         </div>
         
       </div>
-    </div>
+    </main>
   );
 }
 
