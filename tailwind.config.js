12ad35ca964b641e9857aba6154324cd3bcbc377
Arintia <arantiabusiness@gmail.com>
2022-04-23 12:18:12 +0300

    Add theme switcher

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: false
   },
   plugins: [require("daisyui")],
 }
